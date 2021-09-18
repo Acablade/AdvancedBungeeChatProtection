c6d536d804bf3861d2acc4d775470aa8bde97034
oğuz <ogzylmzlr26@yahoo.com>
2021-09-18 18:41:19 +0300

    fixed lower and removed unnecessary imports

--- src/main/java/me/acablade/chatprotection/objects/actions/impl/Lower.java
+++ src/main/java/me/acablade/chatprotection/objects/actions/impl/Lower.java
@@ -5,7 +5,6 @@ import me.acablade.chatprotection.objects.actions.Action;
 import net.md_5.bungee.api.event.ChatEvent;
 
 import java.util.Locale;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
@@ -22,16 +21,6 @@ public class Lower extends Action {
     @Override
     public void execute(ChatEvent event) {
         String message = event.getMessage();
-        Matcher matcher = regex.matcher(message);
-        StringBuilder sb = new StringBuilder();
-        int last = 0;
-        while(matcher.find()){
-            sb.append(message, last, matcher.start());
-            sb.append(matcher.group(0).toLowerCase(new Locale("tr","TR")));
-            last = matcher.end();
-        }
-        sb.append(message.substring(last));
-        message = sb.toString();
-        event.setMessage(message);
+        event.setMessage(message.toLowerCase(new Locale("tr","TR")));
     }
 }
